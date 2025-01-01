dc12854039edb5e273380e6d1e3a7b41ac97c1ee
Raltour <15610159165lmz@gmail.com>
2025-01-01 13:48:33 +0800

    第八次提交 完成Project 1B

---
+++ Project 1B/src/ArrayDeque61B.java
@@ -0,0 +1,105 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayDeque61B<T> implements Deque61B<T> {
+    private T[] items;
+    private int size;
+    private int capacity;
+    private int nextFirst;
+    private int nextLast;
+
+    public ArrayDeque61B() {
+        items = (T[]) new Object[2];
+        size = 0;
+        capacity = 2;
+        nextFirst = 0;
+        nextLast = 1;
+    }
+
+    public void resize(int newCapacity) {
+        T[] newItems = (T[]) new Object[newCapacity];
+        System.arraycopy(items, nextFirst + 1,
+                newItems, 0,
+                capacity - nextFirst - 1);
+        System.arraycopy(items, 0,
+                newItems, capacity - nextFirst - 1,
+                nextLast);
+        capacity = newCapacity;
+        nextFirst = capacity - 1;
+        nextLast = size;
+        items = newItems;
+    }
+
+    @Override
+    public void addFirst(T x) {
+        if (nextFirst == nextLast) {
+            resize(capacity * 2);
+        }
+        items[nextFirst] = x;
+        size++;
+        nextFirst = Math.floorMod(nextFirst - 1, capacity);
+    }
+
+    @Override
+    public void addLast(T x) {
+        if (nextFirst == nextLast) {
+            resize(capacity * 2);
+        }
+        items[nextLast] = x;
+        size++;
+        nextLast = Math.floorMod(nextLast + 1, capacity);
+    }
+
+    @Override
+    public List toList() {
+        List<T> list = new ArrayList<>();
+        int i = nextFirst + 1;
+        while (i != nextLast) {
+            if (i == capacity) {
+                i = 0;
+            }
+            list.add(items[i]);
+            i++;
+        }
+        return list;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (size == 0)
+            return true;
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public T removeFirst() {
+        nextFirst = Math.floorMod(nextFirst + 1, capacity);
+        size--;
+        return items[nextFirst];
+    }
+
+    @Override
+    public T removeLast() {
+        nextLast = Math.floorMod(nextLast - 1, capacity);
+        size--;
+        return items[nextLast];
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        return items[Math.floorMod(nextFirst + index + 1, capacity)];
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        throw new UnsupportedOperationException("No need to implement getRecursive for proj 1b");
+    }
+}
--- Project 1B/tests/ArrayDeque61BTest.java
+++ Project 1B/tests/ArrayDeque61BTest.java
@@ -10,14 +10,87 @@ import static com.google.common.truth.Truth.assertWithMessage;
 
 public class ArrayDeque61BTest {
 
-//     @Test
-//     @DisplayName("ArrayDeque61B has no fields besides backing array and primitives")
-//     void noNonTrivialFields() {
-//         List<Field> badFields = Reflection.getFields(ArrayDeque61B.class)
-//                 .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
-//                 .toList();
-//
-//         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
-//     }
+     @Test
+     @DisplayName("ArrayDeque61B has no fields besides backing array and primitives")
+     void noNonTrivialFields() {
+         List<Field> badFields = Reflection.getFields(ArrayDeque61B.class)
+                 .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
+                 .toList();
 
+         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
+     }
+
+     @Test
+     void testAddFirst() {
+         ArrayDeque61B list = new ArrayDeque61B();
+         list.addFirst(10);
+         list.addFirst(20);
+         list.addFirst(30);
+         list.addFirst(40);
+         list.addFirst(50);
+         assertThat(list.toList()).containsExactly(50, 40, 30, 20, 10).inOrder();
+     }
+
+     @Test
+    void testAddLast() {
+        ArrayDeque61B list = new ArrayDeque61B();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addLast(40);
+        list.addLast(50);
+        assertThat(list.toList()).containsExactly(10, 20, 30, 40, 50).inOrder();
+    }
+
+    @Test
+    void testAddFirstAndLast() {
+         ArrayDeque61B list = new ArrayDeque61B();
+         list.addFirst(10);
+         list.addFirst(20);
+         list.addFirst(30);
+         list.addLast(40);
+         list.addLast(50);
+        assertThat(list.toList()).containsExactly(30, 20, 10, 40, 50).inOrder();
+    }
+
+    @Test
+    void testIsEmpty() {
+         ArrayDeque61B list = new ArrayDeque61B();
+         assertThat(list.isEmpty()).isTrue();
+    }
+
+    @Test
+    void testSize() {
+         ArrayDeque61B list = new ArrayDeque61B();
+         list.addFirst(10);
+         list.addFirst(20);
+         list.addLast(30);
+         list.addLast(40);
+         list.addLast(50);
+         list.removeFirst();
+         list.removeLast();
+         assertThat(list.size()).isEqualTo(3);
+    }
+
+    @Test
+    void testRemoveFirst() {
+         ArrayDeque61B list = new ArrayDeque61B();
+         list.addFirst(10);
+         list.addFirst(20);
+         list.addFirst(30);
+         list.removeFirst();
+         list.removeFirst();
+         assertThat(list.toList()).containsExactly(10).inOrder();
+    }
+
+    @Test
+    void testRemoveLast() {
+         ArrayDeque61B list = new ArrayDeque61B();
+         list.addFirst(10);
+         list.addFirst(20);
+         list.addLast(30);
+         list.addLast(40);
+         list.removeLast();
+         assertThat(list.toList()).containsExactly(20, 10, 30).inOrder();
+    }
 }
