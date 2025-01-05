644dada3984fa45d6bea97be3ef9abc3c396cb99
Raltour <15610159165lmz@gmail.com>
2025-01-05 10:56:58 +0800

    完成Homework 2

--- Homework 2/src/Percolation.java
+++ Homework 2/src/Percolation.java
@@ -2,37 +2,85 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 
 public class Percolation {
-    // TODO: Add any necessary instance variables.
+    public int openNumber;
+    public WeightedQuickUnionUF ufT;
+    public WeightedQuickUnionUF ufTB;
+    public int[] isOpen;
+    public int size;
+    public int top;
+    public int bottom;
 
     public Percolation(int N) {
-        // TODO: Fill in this constructor.
+        if (N <= 0) {
+            throw new IllegalArgumentException("hehe");
+        }
+        openNumber = 0;
+        ufT = new WeightedQuickUnionUF(N * N + 1);
+        ufTB = new WeightedQuickUnionUF(N * N + 2);
+        isOpen = new int[N * N];
+        for (int i = 0; i < N * N; i++) {
+            isOpen[i] = -1;
+        }
+        size = N;
+        top = N * N;
+        bottom = N * N + 1;
+        setTopAndBottom();
     }
 
     public void open(int row, int col) {
-        // TODO: Fill in this method.
+        if (row < 0 || row >= size || col < 0 || col >= size) {
+            throw new IndexOutOfBoundsException("haha");
+        }
+        if (isOpen(row, col + 1)) {
+            ufT.union(row * size + col, row * size + col + 1);
+            ufTB.union(row * size + col, row * size + col + 1);
+        }
+        if (isOpen(row, col - 1)) {
+            ufT.union(row * size + col, row * size + col - 1);
+            ufTB.union(row * size + col, row * size + col - 1);
+        }
+        if (isOpen(row + 1, col)) {
+            ufT.union(row * size + col, (row + 1) * size + col);
+            ufTB.union(row * size + col, (row + 1) * size + col);
+        }
+        if (isOpen(row - 1, col)) {
+            ufT.union(row * size + col, (row - 1) * size + col);
+            ufTB.union(row * size + col, (row - 1) * size + col);
+        }
+
+        isOpen[row * size + col] = 1;
+        openNumber++;
     }
 
     public boolean isOpen(int row, int col) {
-        // TODO: Fill in this method.
-        return false;
+        if (row < 0 || row >= size || col < 0 || col >= size) {
+            return false;
+        } else if (isOpen[row * size + col] == -1) {
+            return false;
+        }
+        return true;
     }
 
     public boolean isFull(int row, int col) {
-        // TODO: Fill in this method.
-        return false;
+        if (!isOpen(row, col)) {
+            return false;
+        }
+        return ufT.connected(top, row * size + col);
     }
 
     public int numberOfOpenSites() {
-        // TODO: Fill in this method.
-        return 0;
+        return openNumber;
     }
 
     public boolean percolates() {
-        // TODO: Fill in this method.
-        return false;
+        return ufTB.connected(top, bottom);
     }
 
-    // TODO: Add any useful helper methods (we highly recommend this!).
-    // TODO: Remove all TODO comments before submitting.
-
+    public void setTopAndBottom() {
+        for (int i = 0; i < size; i++) {
+            ufTB.union(i, top);
+            ufTB.union((size - 1) * size + i, bottom);
+            ufT.union(i, top);
+        }
+    }
 }
--- Homework 2/tests/PercolationTest.java
+++ Homework 2/tests/PercolationTest.java
@@ -8,7 +8,7 @@ public class PercolationTest {
     /**
      * Enum to represent the state of a cell in the grid. Use this enum to help you write tests.
      * <p>
-     * (0) CLOSED: isOpen() returns true, isFull() return false
+     * (0) CLOSED: isOpen() returns false, isFull() return false
      * <p>
      * (1) OPEN: isOpen() returns true, isFull() returns false
      * <p>
@@ -82,7 +82,28 @@ public class PercolationTest {
     //       write some more tests and delete the fail() line
     @Test
     public void yourFirstTestHere() {
-        fail("Did you write your own tests?");
+        int N = 5;
+        Percolation p = new Percolation(N);
+        // open sites at (r, c) = (0, 1), (2, 0), (3, 1), etc. (0, 0) is top-left
+        int[][] openSites = {
+                {0, 1},
+                {2, 0},
+                {3, 1},
+                {4, 1},
+                {1, 0},
+                {1, 1}
+        };
+        Cell[][] expectedState = {
+                {Cell.CLOSED, Cell.FULL, Cell.CLOSED, Cell.CLOSED, Cell.CLOSED},
+                {Cell.FULL, Cell.FULL, Cell.CLOSED, Cell.CLOSED, Cell.CLOSED},
+                {Cell.FULL, Cell.CLOSED, Cell.CLOSED, Cell.CLOSED, Cell.CLOSED},
+                {Cell.CLOSED, Cell.OPEN, Cell.CLOSED, Cell.CLOSED, Cell.CLOSED},
+                {Cell.CLOSED, Cell.OPEN, Cell.CLOSED, Cell.CLOSED, Cell.CLOSED}
+        };
+        for (int[] site : openSites) {
+            p.open(site[0], site[1]);
+        }
+        assertThat(p.isOpen(2, 0)).isTrue();
+        assertThat(p.isFull(4, 1)).isFalse();
     }
-
 }
