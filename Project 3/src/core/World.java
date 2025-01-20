6a3daa2b47a4ec2b5c7aae9fc224778b1ac14a62
Raltour <15610159165lmz@gmail.com>
2025-01-20 11:22:06 +0800

    找到了正确的方向，实现了向右生成新的房间以及走廊 完成了部分向上生成走廊和房间的任务 目前来看，一切顺利，但是后面出现房间、走廊交叉时的状况仍难以估计

--- Project 3/src/core/World.java
+++ Project 3/src/core/World.java
@@ -13,23 +13,127 @@ import static edu.princeton.cs.algs4.StdRandom.uniform;
 public class World {
     public static final int WIDTH = 80;
     public static final int HEIGHT = 30;
-    public static final int BLOCK_SIZE = 10;
+    public static final int BLOCK_SIZE = 10;//在随机生成room时用作一个参考的大小
     public int BLOCK_WIDTH = WIDTH / BLOCK_SIZE;
     public int BLOCK_HEIGHT = HEIGHT / BLOCK_SIZE;
+    public int ROOM_NUM = WIDTH / BLOCK_SIZE + HEIGHT / BLOCK_SIZE;
     public double CONNECT_PROBABILITY = 0.8;
 
     private TETile[][] myworld;
     private int seed;
     Random rand;
 
-    public World() {
+    private World() {
         myworld = new TETile[WIDTH][HEIGHT];
         fillWorldWithBlanck(myworld);
         seed = 1234;
         rand = new Random(seed);
-        this.generateRandomRooms();
-        this.connectRooms();
+        this.randomGenerate();
     }
+
+
+
+    private class Room {
+        private int x1;
+        private int y1;
+        private int x2;
+        private int y2;
+        //0 1 2 3分别代表上下左右
+
+        private Room(int x1, int y1, int x2, int y2) {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+
+        private boolean generateRoom(TETile[][] world) {
+            if (this.x1 < 0 || this.x2 >= WIDTH || this.y1 < 0 || this.y2 >= HEIGHT) {
+                return false;
+            }
+            for (int i = x1; i <= x2; i++) {
+                world[i][y1] = Tileset.WALL;
+                world[i][y2] = Tileset.WALL;
+            }
+            for (int i = y1; i <= y2; i++) {
+                world[x1][i] = Tileset.WALL;
+                world[x2][i] = Tileset.WALL;
+            }
+            for (int i = x1 + 1; i < x2; i++) {
+                for (int j = y1 + 1; j < y2; j++) {
+                    world[i][j] = Tileset.FLOOR;
+                }
+            }
+            return true;
+        }
+
+        private void generateNextRoom(TETile[][] world) {
+            for (int i = 0; i < 3; i++) {
+                int p = uniform(rand, 0, 5);
+                if (p == 0) {
+                    //向右生成横向走廊
+                    int x = x2 + uniform(3, 10);
+                    int y = y1 + uniform(rand, 1, y2 - y1);
+                    int height = uniform(rand, 4, BLOCK_SIZE);
+                    int y11 = uniform(rand, y - height + 2, y);
+                    Room nextRoom = new Room(x, y11, x + uniform(rand, 3, BLOCK_SIZE), y11 + height - 1);
+                    if (nextRoom.generateRoom(world)) {
+                        Hallway hw = new Hallway(x2, y - 1, x, y + 1, 0);
+                        hw.generateHallway(world);
+                        nextRoom.generateNextRoom(world);
+                    }
+                } else if(p == 1) {
+                    //向左生成横向走廊
+                } else if(p == 2) {
+                    //向上生成纵向走廊
+                    int x = x1 + uniform(rand, 1, x2 - x1);
+                    int y = y2 + uniform(rand, 3, 10);
+                    int width = uniform(rand, 4, BLOCK_SIZE);
+                    int x11 = uniform(rand, x - width + 2, x);
+                    Room nextRoom = new Room(x11, y, x11 + width - 1, y + uniform(rand, 3, BLOCK_SIZE));
+                } else if(p == 3) {
+                    //向下生成纵向走廊
+                }
+            }
+        }
+    }
+
+
+
+    private class Hallway {
+        private int x1;
+        private int y1;
+        private int x2;
+        private int y2;
+        private int directions;//0表示横向，1表示纵向
+
+        private Hallway(int x1, int y1, int x2, int y2, int directions) {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+            this.directions = directions;
+        }
+
+        private void generateHallway(TETile[][] world) {
+            if (directions == 0) {
+                for (int i = x1; i <= x2; i++) {
+                    world[i][y1] = Tileset.WALL;
+                    world[i][y2] = Tileset.WALL;
+                    world[i][y1 + 1] = Tileset.FLOOR;
+                }
+            } if (directions == 1) {
+                for (int i = y1; i <= y2; i++) {
+                    world[x1][i] = Tileset.WALL;
+                    world[x2][i] = Tileset.WALL;
+                    world[x1 + 1][i] = Tileset.FLOOR;
+                }
+            }
+        }
+    }
+
+
+
 
     private void fillWorldWithBlanck(TETile[][] myworld) {
         for (int x = 0; x < WIDTH; x++) {
@@ -47,84 +151,16 @@ public class World {
         return myworld;
     }
 
-    public void generateRandomRooms() {
-        for (int x = 0; x < BLOCK_WIDTH; x++) {
-            for (int y = 0; y < BLOCK_HEIGHT; y++) {
-                this.blockRandomRoom(x, y);
-            }
-        }
-    }
 
-    private void blockRandomRoom(int x, int y) {
-        int x_axis = x * BLOCK_SIZE;
-        int y_axis = y * BLOCK_SIZE;
+    private void randomGenerate() {
+        int x1 = uniform(rand, 0, 60);
+        int y1 = uniform(rand, 0, 20);
+        int x2 = x1 + BLOCK_SIZE - uniform(rand, 1, 4);
+        int y2 = y1 + BLOCK_SIZE - uniform(rand, 1, 4);
 
-        int x1_axis = x_axis + uniform(rand, 0, 5);
-        int y1_axis = y_axis + uniform(rand, 0, 5);
-        int x2_axis = x_axis + BLOCK_SIZE - uniform(rand, 1, 4);
-        int y2_axis = y_axis + BLOCK_SIZE - uniform(rand, 1, 4);
-
-        for (int i = x1_axis; i <= x2_axis; i++) {
-            this.myworld[i][y1_axis] = Tileset.WALL;
-            this.myworld[i][y2_axis] = Tileset.WALL;
-        }
-        for (int i = y1_axis; i <= y2_axis; i++) {
-            this.myworld[x1_axis][i] = Tileset.WALL;
-            this.myworld[x2_axis][i] = Tileset.WALL;
-        }
-        for (int i = x1_axis + 1; i < x2_axis; i++) {
-            for (int j = y1_axis + 1; j < y2_axis; j++) {
-                this.myworld[i][j] = Tileset.FLOOR;
-            }
-        }
-    }
-
-    public void connectRooms() {
-        for (int x = 0; x < BLOCK_WIDTH; x++) {
-            for (int y = 0; y < BLOCK_HEIGHT - 1; y++) {
-                if (bernoulli(rand, CONNECT_PROBABILITY)) {
-                    roomConnect(x, y, 0);
-                    //0 代表竖着，1 代表横着
-                }
-            }
-        }
-        for (int y = 0; y < BLOCK_HEIGHT; y++) {
-            for (int x = 0; x < BLOCK_WIDTH - 1; x++) {
-                if (bernoulli(rand, CONNECT_PROBABILITY)) {
-                    roomConnect(x, y, 1);
-                    //0 代表竖着，1 代表横着
-                }
-            }
-        }
-    }
-
-    private void roomConnect(int x, int y, int z) {
-        if (z == 0) {
-            int line = uniform(rand, 1, 8) + x * BLOCK_SIZE;
-            int canConnect = 0;
-            for (int i = y * BLOCK_SIZE; i < (y + 2) * BLOCK_SIZE - 1; i++) {
-                if (this.myworld[line][i] == Tileset.WALL) {
-                    canConnect++;
-                }
-            }
-            if (canConnect == 2) {
-                blockConnect(x, y, z, line);
-            }
-        }
-    }
-
-    private void blockConnect(int x, int y, int z, int line) {
-        if (z == 0) {
-            for (int i = y * BLOCK_SIZE + 7; i < y * BLOCK_SIZE + 14; i++) {
-                if (this.myworld[line][i] == Tileset.WALL) {
-                    this.myworld[line][i] = Tileset.FLOOR;
-                } else if (this.myworld[line][i] == Tileset.NOTHING) {
-                    this.myworld[line][i] = Tileset.FLOOR;
-                    this.myworld[line - 1][i] = Tileset.WALL;
-                    this.myworld[line + 1][i] = Tileset.WALL;
-                }
-            }
-        }
+        Room room = new Room(x1, y1, x2, y2);
+        room.generateRoom(myworld);
+        room.generateNextRoom(myworld);
     }
 
 
