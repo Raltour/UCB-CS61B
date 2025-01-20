e429d6611e8f6dd9b133f19f858257d6432c0868
Raltour <15610159165lmz@gmail.com>
2025-01-20 22:28:59 +0800

    实现了根据玩家输入的种子生成对应世界的功能 下一部为界面添加更多功能 或者是检查输入，增强程序健壮性 或者是调整生成概率，使得界面尽可能被填满一些

--- Project 3/src/core/Main.java
+++ Project 3/src/core/Main.java
@@ -9,10 +9,11 @@ import static core.World.HEIGHT;
 import static core.World.WIDTH;
 import static core.World.DEFAULT_SEED;
 import static edu.princeton.cs.algs4.StdDraw.*;
+import static java.lang.Character.getNumericValue;
 
 public class Main {
     public static void main(String[] args) {
-        StdDraw.setCanvasSize(600, 800);
+        StdDraw.setCanvasSize(800, 600);
         StdDraw.clear(StdDraw.BLACK);
         StdDraw.setPenColor(StdDraw.WHITE);
         Font font = new Font("Arial", Font.BOLD, 40);
@@ -29,9 +30,24 @@ public class Main {
                 int key = nextKeyTyped();
 
                 if (key == 'n' || key == 'N') {
+                    StdDraw.clear(StdDraw.BLACK);
+                    StdDraw.text(0.2, 0.4, "Enter your Seed(enter to end): ");
+                    StringBuilder seed = new StringBuilder();
+                    while (true) {
+                        if (hasNextKeyTyped()) {
+                            key = nextKeyTyped();
+                            if (key == '\n') {
+                                break;
+                            }
+                            seed.append(getNumericValue(key));
+                            StdDraw.clear(StdDraw.BLACK);
+                            StdDraw.text(0.2, 0.4, "Enter your Seed(enter to end): ");
+                            StdDraw.textLeft(0.4, 0.4, seed.toString());
+                        }
+                    }
                     TERenderer ter = new TERenderer();
                     ter.initialize(WIDTH, HEIGHT);
-                    World myWorld = World.createWorld(DEFAULT_SEED);
+                    World myWorld = World.createWorld(Integer.parseInt(seed.toString()));
                     ter.renderFrame(myWorld.getWorld());
 
                     break;
