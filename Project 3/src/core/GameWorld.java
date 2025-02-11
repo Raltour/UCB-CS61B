10141aec768b79d840774819fc6f3d09a4a272a9
Raltour <15610159165lmz@gmail.com>
2025-02-11 14:03:53 +0800

    创建GameWOrld类

---
+++ Project 3/src/core/GameWorld.java
@@ -0,0 +1,26 @@
+package core;
+
+public class GameWorld implements GameState{
+
+
+
+    @Override
+    public void update(int pressed) {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void enter() {
+
+    }
+
+    @Override
+    public void exit() {
+
+    }
+}
